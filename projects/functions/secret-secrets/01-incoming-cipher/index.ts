2ca187bb800150ff8ac9d2d854faa6dea02a39af
vccyb <13170906656@163.com>
2024-01-06 23:45:23 +0800

    1-6-study

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,15 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type Cipher = (character: string) => string;
+
+export function createCipher(cipher: Cipher) {
+	return (text: string) => {
+		let result = "";
+
+		for (const character of text) {
+			result += cipher(character);
+		}
+		return result;
+	};
+}
