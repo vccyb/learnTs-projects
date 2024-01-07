6a09a3e1f5cef1fe0d187eebc8108cbfbcd16ba0
vccyb <13170906656@163.com>
2024-01-07 13:20:51 +0800

    1-7-study

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,20 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function shallowDifferences(a: string[], b: string[]) {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+
+	const result: (string | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] === b[i]) {
+			result.push(a[i]);
+		} else {
+			result.push(undefined);
+		}
+	}
+
+	return result;
+}
